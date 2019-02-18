15df7ef9cd2973b55aa26fe3d9fe400bd7f2f188
huming <huming@szprize.cn>
2019-02-18 18:31:10 +0800

    modify sponsor

--- app/src/main/java/cn/walkpast/horizon/SponsorActivity.java
+++ app/src/main/java/cn/walkpast/horizon/SponsorActivity.java
@@ -2,9 +2,14 @@ package cn.walkpast.horizon;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.walkpast.core.WebHorizonActivity;
 
 /**
  * Author: Kern
@@ -12,8 +17,15 @@ import butterknife.ButterKnife;
  * Description: This is..
  */
 
-public class SponsorActivity extends AppCompatActivity {
+public class SponsorActivity extends WebHorizonActivity implements View.OnClickListener {
 
+
+    private String URL = "https://github.com/KernHu/Horizon/blob/master/app/info/sponsor_list";
+
+    @BindView(R.id.sponsor_back)
+    public ImageView mSponsorBack;
+    @BindView(R.id.sponsor_container)
+    public FrameLayout mSponsorContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,5 +34,16 @@ public class SponsorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sponsor);
         ButterKnife.bind(this);
 
+        getHorizon()
+                .setViewContainer(mSponsorContainer)
+                .preview()
+                .loadUrl(URL);
+    }
+
+
+    @OnClick(R.id.sponsor_back)
+    @Override
+    public void onClick(View v) {
+        finish();
     }
 }
